030379beba6a9a72f04df9d8229826b8243e8d94
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-05-20 21:20:36 +0200

    Option to hide prices

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,16 @@
-let { settings } = await chrome.storage.sync.get("settings");
+let {settings} = await chrome.storage.sync.get("settings");
 const $form = document.querySelector("form");
 const $googleSpreadSheetId = document.getElementById('googleSpreadSheetId');
+const $hideprices = document.getElementById('hidePrices');
 
-if(!settings){
+if (!settings) {
     settings = {};
 }
 $googleSpreadSheetId.value = settings.googleSpreadSheetId || '';
+$hideprices.checked = settings.hidePrices || false;
 
 $form.addEventListener("submit", async () => {
     settings.googleSpreadSheetId = $googleSpreadSheetId.value;
-    await chrome.storage.sync.set({ settings: settings });
+    settings.hidePrices = $hideprices.checked;
+    await chrome.storage.sync.set({settings: settings});
 });
