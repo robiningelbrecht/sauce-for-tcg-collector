88bf57d31d065c18be9cb172ad7a7ef9bb96ac2d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-08-04 09:22:11 +0200

    Select which binder placeholders to print

--- src/Feature/PrintBinderPlaceholdersFeature.js
+++ src/Feature/PrintBinderPlaceholdersFeature.js
@@ -49,27 +49,75 @@ export class PrintBinderPlaceholdersFeature {
 
                 const $placeholder = document.createElement('div');
                 $placeholder.classList.add(...['placeholder', toValidCssClassName(cardName), toValidCssClassName(variantName)]);
+                $placeholder.setAttribute('data-card-id', cardId);
                 $placeholder.innerHTML = `<div>${cardName}</div><div>${cardNumber}</div><div>${variantName}</div>`;
 
                 $printWrapper.appendChild($placeholder);
             });
+
+            // Add a checkbox to select the expansion in print-selection-mode.
+            const $checkbox = document.createElement('input');
+            $checkbox.setAttribute('type', 'checkbox');
+            $checkbox.setAttribute('data-card-id', cardId);
+            $checkbox.addEventListener('click', () => {
+                document.querySelector('button span.count').innerHTML = document.querySelectorAll('input[type="checkbox"][data-card-id]:checked').length;
+            })
+            $card.appendChild($checkbox);
         });
-        document.body.appendChild($printWrapper);
+
+        const $body = document.body;
+        $body.appendChild($printWrapper);
+
+        const $printNavigation = document.createElement('div');
+        $printNavigation.classList.add(...['print-navigation']);
+        const $inner = document.createElement('div');
+        $inner.classList.add(...['inner']);
+        $printNavigation.appendChild($inner);
+        $body.appendChild($printNavigation);
+
+        const $cancelPrintSelectionModeButton = document.createElement('button');
+        $cancelPrintSelectionModeButton.classList.add(...['cancel']);
+        $cancelPrintSelectionModeButton.innerHTML = `<span class="fa-solid fa-ban"></span><div>Exit print mode</div>`;
+        $cancelPrintSelectionModeButton.addEventListener('click', () => {
+            $body.classList.remove('in-print-selection-mode');
+        });
 
         const $printButton = document.createElement('button');
-        $printButton.classList.add(...['button', 'button-plain-alt', 'print']);
-        $printButton.setAttribute('title', 'Print binder placeholders');
-        $printButton.innerHTML = `<span class="fa-solid fa-print"></span><div>Print binder placeholders</div>`;
+        $printButton.classList.add(...['print']);
+        $printButton.innerHTML = `<span class="fa-solid fa-print"></span><div>Print <span class="count">0</span> binder placeholder(s)</div>`;
         $printButton.addEventListener('click', () => {
-            document.body.classList.add('printing');
+            // Hide all unchecked cards.
+            document.querySelectorAll(`div.placeholder[data-card-id]`).forEach($placeholder => {
+                $placeholder.style.display = null;
+            });
+            document.querySelectorAll('input[type="checkbox"][data-card-id]:not(:checked)').forEach($checkbox => {
+                const cardId = $checkbox.getAttribute('data-card-id');
+                document.querySelectorAll(`div.placeholder[data-card-id="${cardId}"]`).forEach($placeholder => {
+                    $placeholder.style.display = 'none';
+                });
+
+            });
+            $body.classList.add('printing');
             window.print();
+        })
+
+        $inner.appendChild($printButton);
+        $inner.appendChild($cancelPrintSelectionModeButton);
+
+        const $togglePrintSelectionModeButton$ = document.createElement('button');
+        $togglePrintSelectionModeButton$.classList.add(...['button', 'button-plain-alt', 'toggle-print-selection']);
+        $togglePrintSelectionModeButton$.setAttribute('title', 'Print binder placeholders');
+        $togglePrintSelectionModeButton$.innerHTML = `<span class="fa-solid fa-print"></span><div>Print binder placeholders</div>`;
+        $togglePrintSelectionModeButton$.addEventListener('click', () => {
+            $body.classList.add('in-print-selection-mode');
         });
 
         const $appendAfter = document.querySelector('div#card-search-result-title');
-        $appendAfter.parentNode.insertBefore($printButton, $appendAfter.nextSibling);
+        $appendAfter.parentNode.insertBefore($togglePrintSelectionModeButton$, $appendAfter.nextSibling);
 
         addEventListener("afterprint", () => {
-            document.body.classList.remove('printing');
+            $body.classList.remove('in-print-selection-mode');
+            $body.classList.remove('printing');
         });
 
     }
