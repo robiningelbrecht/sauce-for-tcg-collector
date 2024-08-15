4a46265327750e975076a0c24b86b7ce597b9c72
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-08-15 17:11:02 +0200

    show progress bar on toast

--- src/Component/Toast.js
+++ src/Component/Toast.js
@@ -24,8 +24,9 @@ export class Toast {
             close: true,
             gravity: "bottom", // `top` or `bottom`
             position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            escapeMarkup: false
+            stopOnFocus: false, // Prevents dismissing of toast on hover
+            escapeMarkup: false,
+            progressBar: true,
         }).showToast();
     }
 
