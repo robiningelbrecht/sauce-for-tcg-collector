9526efc46bd072d6dd27b368dac2b38489a85c8d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-05-06 11:05:19 +0200

    Removed price calculations

--- src/Feature/DashboardFeature.js
+++ src/Feature/DashboardFeature.js
@@ -1,9 +1,6 @@
-import {AppState} from "../Infrastructure/AppState";
-
 export class DashboardFeature {
-    constructor(settings, JapaneseCardPriceRepository) {
+    constructor(settings) {
         this.settings = settings;
-        this.japaneseCardPriceRepository = JapaneseCardPriceRepository;
     }
 
     getId = () => {
@@ -19,31 +16,9 @@ export class DashboardFeature {
     }
 
     apply = async () => {
-        const appState = AppState.fromHtml();
-
         const [$firstDashboardSectionsRegion, $secondDashboardSectionsRegion] = document.querySelectorAll('div.dashboard-sections-region');
         $firstDashboardSectionsRegion.querySelector('div#dashboard-sets-completed-section').style.display = 'none';
         $secondDashboardSectionsRegion.style.display = 'none';
         $firstDashboardSectionsRegion.appendChild($secondDashboardSectionsRegion.querySelectorAll('div.dashboard-sections-subregion')[1].querySelector('div.dashboard-section'));
-
-        if (this.settings.hidePrices()) {
-            return;
-        }
-        if (!this.settings.getJapaneseCardPricesUrl()) {
-            return;
-        }
-
-        if (appState.isJapaneseTcgRegionContext()) {
-            // Set real total market price.
-            const totalCollectionValue = await this.japaneseCardPriceRepository.findTotalCollectionValueFor('jp');
-            const $marketPrice = document.querySelectorAll('#dashboard-cards .dashboard-card')[3].querySelector('.dashboard-card-text');
-            $marketPrice.innerHTML = `${totalCollectionValue.toFormat('$0')}`;
-        }
-        if (appState.isAllTcgRegionContext()) {
-            const totalCollectionValue = await this.japaneseCardPriceRepository.findTotalCollectionValue();
-            const $marketPrice = document.querySelectorAll('#dashboard-cards .dashboard-card')[3].querySelector('.dashboard-card-text');
-
-            $marketPrice.innerHTML = `${totalCollectionValue.toFormat('$0')}`;
-        }
     }
 }
--- src/Infrastructure/Container.js
+++ src/Infrastructure/Container.js
@@ -1,4 +1,3 @@
-import {JapaneseCardPriceRepository} from "../Domain/JapaneseCardPriceRepository";
 import {NewMenuItemFeature} from "../Feature/NewMenuItemFeature";
 import {HidePricesFeature} from "../Feature/HidePricesFeature";
 import {DashboardFeature} from "../Feature/DashboardFeature";
@@ -7,30 +6,26 @@ import {PurchasePriceFeature} from "../Feature/PurchasePriceFeature";
 import {PrintBinderPlaceholdersFeature} from "../Feature/PrintBinderPlaceholdersFeature";
 import {MarketPlaceLinksFeature} from "../Feature/MarketPlaceLinksFeature";
 import {Settings} from "./Settings";
-import {DisplayJapanesePricesFeature} from "../Feature/DisplayJapanesePricesFeature";
 import {PrintBinderExpansionLogosFeature} from "../Feature/PrintBinderExpansionLogosFeature";
 import {FilterExpansionsFeature} from "../Feature/FilterExpansionsFeature";
 
 const settings = await Settings.fromSyncStorage();
-const japaneseCardPriceRepository = new JapaneseCardPriceRepository(settings);
 
 const features = [
     new NewMenuItemFeature(settings),
     new HidePricesFeature(settings),
-    new DashboardFeature(settings, japaneseCardPriceRepository),
+    new DashboardFeature(settings),
     new QuickAccessLinksFeature(settings),
     new PurchasePriceFeature(settings),
     new PrintBinderPlaceholdersFeature(),
     new PrintBinderExpansionLogosFeature(),
     new MarketPlaceLinksFeature(settings),
-    new DisplayJapanesePricesFeature(settings, japaneseCardPriceRepository),
     new FilterExpansionsFeature()
 ];
 
 
 const Container = {
     Settings: settings,
-    TcgCardPriceRepository: japaneseCardPriceRepository,
     Features: features
 }
 
